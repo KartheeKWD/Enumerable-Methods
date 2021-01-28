b04fa6eda2e545157aea6038864a7d7426f8af19
KartheeK Paturu <kartheekth@gmail.com>
2021-01-28 16:21:38 +0530

    Implemented each and each with index

--- enumerables.rb
+++ enumerables.rb
@@ -1,21 +1,30 @@
-# rubocop:disable Style/CaseEquality
+# # rubocop:disable Style/CaseEquality
 module Enumerable
-
     def my_each
         return to_enum(:my_each) unless  block_given?
-        arr = arr.to_a
+        arr = self.is_a?(Array) ? self : self.to_a
         i = 0
-        while i < arr.length
-            yield arr[i]  
+        # puts arr
+        while i < arr.length do
+            yield arr[i]
+            i += 1
         end
-        self 
-    end 
+        self
+    end
+
+    def my_each_with_index
+        return to_enum(:my_each_with_index) unless  block_given?
+        arr = self.is_a?(Array) ? self : self.to_a
+        i = 0
+        # puts arr
+        while i < arr.length do
+            yield  arr[i], i
+            i += 1
+        end
+        self
+    end
 end
 
- a = [1, 2,3,4]
- puts a.my_each {|i| puts i}
-
-
-
-
-  
+a = [1, 2,3,4]
+p a.my_each{ |i| puts i}
+p a.my_each_with_index { |item ,index| p "#{item} is at index #{index}" }
