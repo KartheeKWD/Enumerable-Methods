f6a90c84c3167fde07fa1a52247dad53a1e28aa0
Arinpe <dehtolah@gmail.com>
2021-02-01 15:19:10 +0100

    implemented my_all, my_any and my_none

--- enumerables.rb
+++ enumerables.rb
@@ -35,18 +35,42 @@ module Enumerable
        return new_array
     end 
 
-    def my_all?
-        return to_enum(:my_all?) unless  block_given?
-        arr = self.to_a
-        arr.each do |i|
+    # def my_all?
+    #     return to_enum(:my_all?) unless  block_given?
+    #     arr = self.to_a
+    #     arr.each do |i|
           
-        end 
-    end
+    #     end 
+    # end
+
+    def my_all?
+        my_each{ |item| return false if yield(item) == false}
+        true
+      end
+
+      def my_any?
+        my_each{ |item| return true if yield(item) == true}
+        false
+      end
+
+      def my_none?
+        my_each{ |item| return false if yield(item) !=false}
+        true
+      end
+
+      def my_count
+
 end
 
 a = [1, 2,3,4]
-# p a.my_each{ |i| puts i}
-# p a.my_each_with_index { |item ,index| p "#{index} : #{item}" }
-# p a.my_select {|item|  item == 2 }
+p a.my_each{ |i| puts i}
+p a.my_each_with_index { |item ,index| p "#{index} : #{item}" }
+p a.my_select {|item|  item == 2 }
 p a.my_all? {|item|  item == 2 }
 p a.all? {|item|  item == 6 }
+p a.any? {|item|  item <=2 }
+p a.my_any? {|item|  item <= 2 }
+p a.my_none? {|item|  item ==4 }
+p a.none? {|item|  item ==4 }
+
+ 
