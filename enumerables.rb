e949d3507ca9f19d8ecd05ae125244d9113fd004
KartheeK Paturu <kartheekth@gmail.com>
2021-02-04 21:07:38 +0530

    inject method

--- enumerables.rb
+++ enumerables.rb
@@ -64,24 +64,53 @@ module Enumerable
         a = self.class == Array ? self :to_a
         a.my_each {|item| yield(item)}
     end 
-
-
-
+    
+    # // If no block is given and an arithmetic symbol or string is given without an initial value
+    # // If no block is given and an arithmetic symbol or string is given without an initial value
+    
+    def my_inject(*args)
         
-
+        initial_value, symbol = args if args.length == 2
+        symbol = args[0] if args.length == 1 && !block_given?
+        initial_value = args[0] if args.length == 1 && block_given?
+        
+        return raise LocalJumpError, 'no block provided' if !block_given? && symbol.nil?
+        
+        if block_given?
+            each do |i|
+                if initial_value.nil?
+                    initial_value = i
+                    next
+                end
+                initial_value = yield(initial_value, i)
+            end
+        else 
+            if (symbol && symbol.class == Symbol) || (symbol && symbol.class == String && %r{[-+*/]}.match(symbol))
+                each do |i|
+                    if initial_value.nil?
+                        initial_value = i
+                        next
+                    end
+                    initial_value = initial_value.send(symbol, i)
+                end 
+            end
+        end
+        initial_value
+    end
 end
 
-a = [1, 2,3,4]
-p a.my_each{ |i| puts i}
-p a.my_each_with_index { |item ,index| p "#{index} : #{item}" }
-p a.my_select {|item|  item == 2 }
-p a.my_all? {|item|  item == 2 }
-p a.all? {|item|  item == 6 }
-p a.any? {|item|  item <=2 }
-p a.my_any? {|item|  item <= 2 }
-p a.my_none? {|item|  item ==4 }
-p a.none? {|item|  item ==4 }
-p a.count(2) 
-p a.map { |item| item * 2 }
+a = [1,2,3,4]
+p a.my_inject("=" ) 
+# p a.my_each{ |i| puts i}
+# p a.my_each_with_index { |item ,index| p "#{index} : #{item}" }
+# p a.my_select {|item|  item == 2 }
+# p a.my_all? {|item|  item == 2 }
+# p a.all? {|item|  item == 6 }
+# p a.any? {|item|  item <=2 }
+# p a.my_any? {|item|  item <= 2 }
+# p a.my_none? {|item|  item ==4 }
+# p a.none? {|item|  item ==4 }
+# p a.count(2) 
+# p a.map { |item| item * 2 }
 
  
